403a113ae1cb3d8a3fb1b857bee626193dbf540f
shsioi <dragonbass49@gmail.com>
2026-01-06 23:12:08 +0900

    lab12_1comp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int N){
+    
+    
+    if (N==0){
+        
+        return 0;
+    
+    }else if (N==1){
+        
+        return 1;
+        
+    }else {
+        N = fibonacci(N-1) + fibonacci(N-2);
+        return N;
+        
+    }
+    
 }
